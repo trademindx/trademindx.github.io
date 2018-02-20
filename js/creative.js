47abbb33168138feba7d5fd66dff5b5c6406ccf4
Alex Petlenko <alex@trademindx.com>
2018-02-20 18:04:36 +0000

    fix link highlighting

--- js/creative.js
+++ js/creative.js
@@ -23,7 +23,7 @@
   // Activate scrollspy to add active class to navbar items on scroll
   $('body').scrollspy({
     target: '#mainNav',
-    offset: 57
+    offset: 70
   });
 
   // Collapse Navbar
